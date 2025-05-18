6922cb7424d6047339acd0af64ab8ec271f2b471
burstlimit <yourewelcomex@gmail.com>
2025-05-18 19:08:36 +0300

    initial commit

---
+++ MusicPlayerWPF/GradientMaker.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace MusicPlayerWPF
+{
+    public static class GradientMaker
+    {
+        public static LinearGradientBrush GetGradientFromImage(string imagePath)
+        {
+            return new();
+        }
+    }
+}
